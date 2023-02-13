57d2e46c9757e603aec6d9396a365d61deb6f9f5
Ricard Torres <torres.rick@gmail.com>
2023-02-13 15:36:25 +0100

    Add Mastodon API support

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Mastodon post scheduler
 
-It allows you to schedule a list of posts to be published on Mastodon.
+It allows you to "schedule" (or "publish later") a list of posts to be published on Mastodon. It doesn't actually use the schedule feature.
 
 It uses GitHub actions to run the script every 1 hour. The script will check if there is a post to be published and will publish it. It will only publish 1 post at a time.
--- schedule.py
+++ schedule.py
@@ -2,7 +2,22 @@
 # encoding=utf8
 
 import os
+import argparse
 import json
+from mastodon import Mastodon
+
+# parse arguments
+parser = argparse.ArgumentParser (description = 'Generate an HTML archive of a mastodon user.')
+parser.add_argument ('--instance', required=True, help='url to your instance')
+parser.add_argument ('--access-token', required=True, help='token providing access to your account')
+args = parser.parse_args()
+
+# connect to mastodon
+mstdn = Mastodon(
+		access_token = args.access_token,
+		api_base_url = args.instance
+		)
+user = mstdn.account_verify_credentials();
 
 fileName = 'posts.json'
 
@@ -12,13 +27,16 @@ with open(fileName, 'r') as f:
   # Only post the latest available
   if len(posts) > 0:
     # We only want to post 1 item at a time
-    status = "@ricard " + posts[0]
-    print(status)
+    mstdn.status_post(
+      status = posts[0]
+      visibility = "public",
+      language = "en"
+    )
 
-  # Remove the item we just posted from the list of posts
-  posts = posts[1:]
+    # Remove the item we just posted from the list of posts
+    posts = posts[1:]
 
-  # Update the file with the posts
-  with open(fileName, 'w') as f:
-    f.write(json.dumps({'posts': posts}))
+    # Update the file with the posts
+    with open(fileName, 'w') as f:
+      f.write(json.dumps({'posts': posts}))
 
